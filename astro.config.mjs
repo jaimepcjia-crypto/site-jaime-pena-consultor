7ed01247ea63e1695e849c0760013b909d4fe086
Jaime PCJ <jaimepcj.ia@gmail.com>
2026-06-01 23:33:47 -0300

    config: migra dominio canonical para jaimepena-mouradubeux.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://jaimepenacaljunior.com.br',
+  site: 'https://www.jaimepena-mouradubeux.com',
   integrations: [tailwind()],
   build: {
     inlineStylesheets: 'auto',
